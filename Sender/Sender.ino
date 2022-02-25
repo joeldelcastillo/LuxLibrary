6f5475cbb9d2021287a44593b37102eefd447b05
gabosalazarn <gabosalazarn@vt.edu>
2022-02-25 11:10:02 -0500

    Added LED Indicator
    LED on when connected
    LED off when disconnected

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -11,14 +11,14 @@ const char* password = "123456789";
 String serverNameCounter = "http://192.168.4.1/b0";
 
 unsigned long previousMillis = 0;
-const long interval = 500; 
+const long interval = 500;
 HTTPClient http;
 float value = 0;
 
 void setup() {
+  pinMode(LED_BUILTIN,OUTPUT);
 
-  
-  Serial.begin(115200); 
+  Serial.begin(115200);
 
   //MPU connection
   Wire.begin();
@@ -32,33 +32,34 @@ void setup() {
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
+  digitalWrite(LED_BUILTIN,HIGH);
+  
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 
 }
 
 void loop() {
-  
+
   unsigned long currentMillis = millis();
   mpu.update();
-  
-  if(currentMillis - previousMillis >= interval) {
-     if(WiFi.status()== WL_CONNECTED){
+
+  if (currentMillis - previousMillis >= interval) {
+    if (WiFi.status() == WL_CONNECTED) {
       //POST request
       String serverPath = httpPOSTRequest(serverNameCounter, "count", mpu.getYaw());
-      
+
       http.begin(serverPath.c_str());
       // Send HTTP GET request
       int httpResponseCode = http.GET();
 
-      if (httpResponseCode>0) {
+      if (httpResponseCode > 0) {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
         String payload = http.getString();
@@ -72,16 +73,17 @@ void loop() {
     }
     else {
       Serial.println("WiFi Disconnected");
+      digitalWrite(LED_BUILTIN,LOW);
     }
     previousMillis = currentMillis;
   }
-    
+
 }
 
 
 //Crea un sring para enviar por parámetros cada valor
 String httpPOSTRequest(String serverName, String param, float value) {
-    String serverPath = serverName + "?" + param + "=" + String(value, 3);
-    Serial.println(param + "=" + String(value, 3));
-    return serverPath;
+  String serverPath = serverName + "?" + param + "=" + String(value, 3);
+  Serial.println(param + "=" + String(value, 3));
+  return serverPath;
 }
