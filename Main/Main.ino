8bebefe528f072f156072838448b1ea6ce4e938c
Joel del Castillo <joel.del.castillo09@gmail.com>
2022-03-03 10:21:08 -0500

    Send, recieve and save raw, pitch and roll

--- Main/Main.ino
+++ Main/Main.ino
@@ -6,7 +6,9 @@
 Sphere light;
 Counter helper;
 MainServer accessPoint("accessPoint");
+int counter = 0;
 AsyncWebServer server(80);
+float artistas[3] = {0.0, 0.0, 0.0};
 
 //Direcciones de cada bailarin
 const char dir0[] = "/b0";
@@ -24,26 +26,32 @@ void setup() {
     helper.increment();
     //Colocar un listener por cada dirección de cada baliarin (cliente)
     server.on(direcciones[i], HTTP_GET, [](AsyncWebServerRequest * request) {
-      float value;
       int artista = helper.getCount();
       //Añadir otro parametro por cada valor a recoger (yaw, pitch ...)
       if (request->hasParam("yaw")) {
         //recoge cada valor de un parámetro
-        String yawS = request->getParam("yaw")->value();;
+        String yawS = request->getParam("yaw")->value();
         String pitchS = request->getParam("pitch")->value();
         String rollS = request->getParam("roll")->value();
 
         float yaw = yawS.toFloat();
         float pitch = pitchS.toFloat();
-        //float roll = rollS.toFloat();
-        float roll = 0;
-        print_roll_pitch_yaw(yaw, pitch, roll);
+        float roll = rollS.toFloat();
+
+        artistas[0] = yaw;
+        artistas[1] = pitch;
+        artistas[2] = roll;
+        
+        
+        //print_roll_pitch_yaw(yaw, pitch, roll);
         //guardar los datos en la clase MainServer
-        accessPoint.setArtista(value, artista, 0);
-        Serial.println(value);
+        //accessPoint.setArtista(yaw, artista, 0);
+        //accessPoint.setArtista(pitch, artista, 1);
+        //accessPoint.setArtista(roll, artista, 2);
+        Serial.println();
       } else {
         //Valor de error
-        value = 999;
+        Serial.println("Error");
       }
       request->send_P(200, "text/plain", "Bien");
     });
@@ -55,20 +63,34 @@ void setup() {
 
 void loop() {
   //Animaciones
-
-
+  //accessPoint.printArtista(0);
   //light.simpleColor(160);
-  //Serial.println("hola");
+  //Serial.println(counter);
+ 
   //light.rainbow(counter);
+  //counter += 1;
+  printArtista();
+  //accessPoint.setArtista(counter, 0, 0);
+  //Serial.println(accessPoint.getArtista(0, 0));
   //light.simpleChangePaletteGyro(accessPoint.getArtista(0)[0]);
-  //delay(1);
+  //Serial.println(counter);
+  delay(5);
 }
 
 void print_roll_pitch_yaw(float yaw, float pitch, float roll) {
-  Serial.print("Yaw, Pitch, Roll: ");
   Serial.print(yaw, 2);
-  Serial.print(", ");
+  Serial.print(",");
   Serial.print(pitch, 2);
-  Serial.print(", ");
+  Serial.print(",");
   Serial.println(roll, 2);
+}
+
+void printArtista(){
+  Serial.print("Artista: ");
+  Serial.print(0);
+  for(int j = 0; j < 3; j++){
+      Serial.print("    ");
+      Serial.print(artistas[j]);
+    }
+    Serial.println();
 }
